b99dcc87cb1644d3c4edfbbc477f75bba08f2286
DianaPaliy <paliy.diana@gmail.com>
2013-05-16 18:31:47 +0400

    less

---
+++ homework/hw_1/js/hide.js
@@ -0,0 +1,12 @@
+function showStuff(id) {
+		document.getElementById(id).style.display = 'block';
+	}
+function hideStuff(id) {
+		document.getElementById(id).style.display = 'none';
+	}
+function reName(id) {
+		document.getElementById(id).className = 'blur_cont';
+	}
+function reNameBack(id) {
+		document.getElementById(id).className = 'cont1';
+	}
