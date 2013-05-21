5b65c68edf3694314528fd31992d6697e93e6755
DianaPaliy <paliy.diana@gmail.com>
2013-05-21 15:49:42 +0400

    last

--- homework/hw_1/js/hide.js
+++ homework/hw_1/js/hide.js
@@ -1,12 +1,17 @@
-function showStuff(id) {
-		document.getElementById(id).style.display = 'block';
+jQuery(document).ready(function(){
+	jQuery('#enter').on('click', function(){
+		jQuery('#overlayId').css('display', 'block');
+		jQuery('#bg_blur').css('display', 'block');
+		jQuery('#cont').attr({
+			'class' : 'blur_cont'
+		});
 	}
-function hideStuff(id) {
-		document.getElementById(id).style.display = 'none';
-	}
-function reName(id) {
-		document.getElementById(id).className = 'blur_cont';
-	}
-function reNameBack(id) {
-		document.getElementById(id).className = 'cont1';
-	}
+	);
+	jQuery('#close').on('click', function(){
+		jQuery('#overlayId').css('display', 'none');
+		jQuery('#bg_blur').css('display', 'none');
+		jQuery('#cont').attr({
+			'class' : 'cont1'
+		});
+	});
+});
