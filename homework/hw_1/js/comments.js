b93656adde6f1652700efdb784fa0d98532d7824
DianaPaliy <paliy.diana@gmail.com>
2013-05-29 23:26:58 +0400

    comm2

--- homework/hw_1/js/comments.js
+++ homework/hw_1/js/comments.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
                 }
             });
             $(this).parent().append($('#addComment').clone());
+             $('#addComment').submit(validate);
             $(this).next().show();
         });
     });
@@ -14,37 +15,34 @@ $(document).ready(function(){
 $(function(){
 
         var field = new Array("author", "text");
-                
-        $("form").submit(function() {
-            var error=0; 
-            $("form").find(":input").each(function() {
-                for(var i=0;i<field.length;i++){ 
-                    if($(this).attr("name")==field[i]){ 
-                        
-                        if(!$(this).val()){
-                            $(this).css('border', 'red 1px solid');
-                            error=1;
-                                                        
-                        }
-                        else{
-                            $(this).css('border', 'gray 1px solid');
-                        }
-                        
-                    }               
+
+        var validate = (function(event){
+            var error=0;        
+            $(event.target).find(":input").each(function() {
+                    for(var i=0;i<field.length;i++){ 
+                        if($(this).attr("name")==field[i]){ 
+                            
+                            if(!$(this).val()){
+                                $(this).css('border', 'red 1px solid');
+                                error=1;
+                                                            
+                            }
+                            else{
+                                $(this).css('border', 'gray 1px solid');
+                            }
+                            
+                        }               
+                    }
+            });
+               
+                if(error==0){ 
+                    return true;
                 }
-           })
-           
-            if(error==0){ 
-                return true;
-            }
-            else{
-            if(error==1) var err_text = "✖  Пожалуйста, заполние это поле!";
-            $("#error").html(err_text); 
-            $("#error").fadeIn("slow"); 
-            return false; 
-            }
-            
-            
-                
-        })
-    });
+                else{
+                if(error==1) var err_text = "✖  Пожалуйста, заполние это поле!";
+                $("#error").html(err_text); 
+                $("#error").fadeIn("slow"); 
+                return false; 
+                };      
+            });
+        });
